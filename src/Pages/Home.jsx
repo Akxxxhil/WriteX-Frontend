811fdcf9451160e98c400be7b4d14a9eec3d7167
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-13 07:32:44 +0530

    copy button added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { allblogList } from "../api/Blog";
+import { MdOutlineContentCopy } from "react-icons/md";
 import Spinner from "../Components/Spinner"
+import toast from 'react-hot-toast';
 
 function Home() {
     const [blogs, setBlogs] = useState([]);
@@ -17,14 +19,26 @@ function Home() {
         fetchBlogs();
     }, []);
 
+    function copyhandler(text){
+       navigator.clipboard.writeText(text).then(()=>{
+        toast.success("copied")
+       }).catch((error)=>{
+        console.error('Failed to copy text: ', error);
+       })
+        
+    }
+
     return (
         <div className='m-6 px-6 py-3'>
             {blogs.length > 0 ? (
                 blogs.map((item, index) => {
                     return (
                         <div key={index} className='my-5'>
-                                <div className='text-3xl font-bold inter'>{item.title}</div>
-                                <div className='inter'>{item.description}</div>
+                            <div className='text-3xl font-bold inter'>{item.title}</div>
+                            <div className='flex items-center justify-between'>
+                                <div  className='inter'>{item.description}</div>
+                                <div onClick={()=>{copyhandler(item.description)}} className='cursor-pointer'><MdOutlineContentCopy size={"20px"} /></div>
+                            </div>
                         </div>
                     );
                 })
