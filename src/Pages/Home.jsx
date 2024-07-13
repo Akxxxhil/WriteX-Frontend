8716ea89f904d04daa962bcf58d359a512e8ef95
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-13 08:08:03 +0530

    date updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { allblogList } from "../api/Blog";
 import { MdOutlineContentCopy } from "react-icons/md";
-import Spinner from "../Components/Spinner"
+import Spinner from "../Components/Spinner";
+import { FaRegHeart } from "react-icons/fa";
 import toast from 'react-hot-toast';
 
 function Home() {
@@ -21,11 +22,10 @@ function Home() {
 
     function copyhandler(text){
        navigator.clipboard.writeText(text).then(()=>{
-        toast.success("copied")
+        toast.success("Copied");
        }).catch((error)=>{
         console.error('Failed to copy text: ', error);
-       })
-        
+       });
     }
 
     return (
@@ -33,11 +33,15 @@ function Home() {
             {blogs.length > 0 ? (
                 blogs.map((item, index) => {
                     return (
-                        <div key={index} className='my-5'>
+                        <div key={index} className='my-5 border-b border-gray-300 pb-2 flex flex-col gap-2'>
                             <div className='text-3xl font-bold inter'>{item.title}</div>
                             <div className='flex items-center justify-between'>
-                                <div  className='inter'>{item.description}</div>
-                                <div onClick={()=>{copyhandler(item.description)}} className='cursor-pointer'><MdOutlineContentCopy size={"20px"} /></div>
+                                <div className='inter'>{item.description}</div>
+                                <div onClick={() => { copyhandler(item.description) }} className='cursor-pointer'><MdOutlineContentCopy size={"20px"} /></div>
+                            </div>
+                            <div className='flex items-center gap-10'>
+                                <div>{new Date(item.createdAt).toDateString()}</div>
+                                <div className='cursor-pointer'><FaRegHeart size={"18px"} /></div>
                             </div>
                         </div>
                     );
