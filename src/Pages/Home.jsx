2eaf1070a57a9a4efb919e8d1a5ef1e9f45554b0
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-16 15:21:56 +0530

    search input

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,13 @@ import { MdOutlineContentCopy } from "react-icons/md";
 import Spinner from "../Components/Spinner";
 import { FaRegHeart } from "react-icons/fa";
 import toast from 'react-hot-toast';
-import { AiOutlineRetweet } from "react-icons/ai";
 import { BiUpvote } from "react-icons/bi";
+import { PiBookmarkSimple } from "react-icons/pi";
 
 function Home() {
     const [blogs, setBlogs] = useState([]);
     const [likedBlogs, setLikedBlogs] = useState({});
+    
 
     useEffect(() => {
         const fetchBlogs = async () => {
@@ -23,12 +24,12 @@ function Home() {
         fetchBlogs();
     }, []);
 
-    function copyhandler(text){
-       navigator.clipboard.writeText(text).then(()=>{
-        toast.success("Copied");
-       }).catch((error)=>{
-        console.error('Failed to copy text: ', error);
-       });
+    function copyhandler(text) {
+        navigator.clipboard.writeText(text).then(() => {
+            toast.success("Copied");
+        }).catch((error) => {
+            console.error('Failed to copy text: ', error);
+        });
     }
 
     function handleLike(index) {
@@ -39,37 +40,48 @@ function Home() {
     }
 
     return (
-        <div className='m-6 px-6 py-3'>
-            {blogs.length > 0 ? (
-                blogs.map((item, index) => {
-                    return (
-                        <div key={index} className='my-5 border-b border-gray-300 pb-2 flex flex-col gap-2'>
-                            <div className='text-3xl font-bold inter'>{item.title}</div>
-                            <div className='flex items-center justify-between'>
-                                <div className='inter'>{item.description}</div>
-                                <div onClick={() => { copyhandler(item.description) }} className='cursor-pointer'><MdOutlineContentCopy size={"20px"} /></div>
-                            </div>
-                            <div className='flex items-center gap-10'>
-                                <div>{new Date(item.createdAt).toDateString()}</div>
-                                <div 
-                                    onClick={() => handleLike(index)} 
-                                    className={`cursor-pointer p-2 rounded-full ${likedBlogs[index] ? 'bg-red-600 text-white' : 'bg-white text-red-600'}`}
-                                >
-                                    <FaRegHeart size={"18px"} />
+        <>
+            <div className="w-full max-w-xs">
+                <input
+                
+                    type="text"
+                    placeholder="Search"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:border-transparent"
+                />
+            </div>
+
+            <div className='m-6 px-6 py-3'>
+                {blogs.length > 0 ? (
+                    blogs.map((item, index) => {
+                        return (
+                            <div key={index} className='my-5 border-b border-gray-300 pb-2 flex flex-col gap-2'>
+                                <div className='text-3xl font-bold inter'>{item.title}</div>
+                                <div className='flex items-center justify-between'>
+                                    <div className='inter'>{item.description}</div>
+                                    <div onClick={() => { copyhandler(item.description) }} className='cursor-pointer'><MdOutlineContentCopy size={"20px"} /></div>
                                 </div>
-                                <div className='cursor-pointer' title='Retweet'><AiOutlineRetweet size={"18px"} /></div>
-                                <div className='cursor-pointer' title='Upvote'><BiUpvote size={"18px"} /></div>
+                                <div className='flex items-center gap-10'>
+                                    <div>{new Date(item.createdAt).toDateString()}</div>
+                                    <div
+                                        onClick={() => handleLike(index)}
+                                        className={`cursor-pointer p-2 rounded-full ${likedBlogs[index] ? 'bg-red-600 text-white' : 'bg-white text-red-600'}`}
+                                    >
+                                        <FaRegHeart size={"18px"} />
+                                    </div>
+                                    <div className='cursor-pointer' title='Retweet'><PiBookmarkSimple size={"20px"} /></div>
+                                    <div className='cursor-pointer' title='Upvote'><BiUpvote size={"18px"} /></div>
+                                </div>
                             </div>
-                        </div>
-                    );
-                })
-            ) : (
-                <div className='flex flex-col items-center justify-center h-screen'>
-                    <div className='flex justify-center items-center'><Spinner /></div>
-                    Blogs are Loading...
-                </div>
-            )}
-        </div>
+                        );
+                    })
+                ) : (
+                    <div className='flex flex-col items-center justify-center h-screen'>
+                        <div className='flex justify-center items-center'><Spinner /></div>
+                        Blogs are Loading...
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
 
